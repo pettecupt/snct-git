b6586748f7c86d5316e6fc8939983e0636139688
Christyan95 <chrisaraujo124@gmail.com>
2023-10-24 16:57:41 -0300

    commit chirs

--- utils/data.ts
+++ utils/data.ts
@@ -1,11 +1,11 @@
 import Profile from '../public/portrait.jpeg';
-import Pedrao from '../public/pedrao.jpeg'
+import Christyan from '../public/christyan.jpeg'
 export const cardData = [
   {
-    id: 0,
-    name: 'Nash',
-    age: 20,
-    src: Profile,
+    id: 2,
+    name: 'Christyan',
+    age: 21,
+    src: Christyan,
     bio: 'Jack of all, Master of some',
     genre: ['Metalcore', 'Pop', 'Rap'],
     tracks: [
@@ -19,26 +19,6 @@ export const cardData = [
         artist: 'Of Mice & Men',
         img: 'https://i.scdn.co/image/ab67616d0000b2738eaa8ea5ed8b61cfeeda605f',
       },
-      {
-        name: "Tears Don't Fall",
-        artist: 'Bullet For my Valentine',
-        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
-      },
-      {
-        name: 'FEEL NOTHING',
-        artist: 'The Plot in You',
-        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
-      },
-    ],
-  },
-  {
-    id: 1,
-    name: 'dudu',
-    age: 2,
-    src: Pedrao,
-    bio: 'Jack of all, Master of some',
-    genre: ['Metalcore', 'Pop', 'Rap'],
-    tracks: [
       {
         name: "Tears Don't Fall",
         artist: 'Bullet For my Valentine',
