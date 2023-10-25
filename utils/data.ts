cd42edca8b8fd0cc1a43e87948265c72aa5430f4
unknown <victorplaymelo09@gmail.com>
2023-10-25 15:34:30 -0300

    feito

--- utils/data.ts
+++ utils/data.ts
@@ -1,5 +1,6 @@
 import Profile from '../public/portrait.jpeg';
-import Pedrao from '../public/pedrao.jpeg'
+import Pedrao from '../public/pedrao.jpeg';
+import Smurfcat from '../public/smurfcat.jpg';
 export const cardData = [
   {
     id: 0,
@@ -48,6 +49,26 @@ export const cardData = [
         name: 'FEEL NOTHING',
         artist: 'The Plot in You',
         img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Melo',
+    age: 18,
+    src: Smurfcat,
+    bio: 'Jack of all, Master of some',
+    genre: ['Metalcore', 'Pop', 'Rap'],
+    tracks: [
+      {
+        name: 'Xo tour Lif3',
+        artist: 'Lil Uzi Vert',
+        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
+      },
+      {
+        name: 'Outcast',
+        artist: 'Mainland',
+        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
       },
     ],
   }
