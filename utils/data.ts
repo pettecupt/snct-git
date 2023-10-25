26cdd4f924cbebe9c73b1bfdf78e287dc225509c
Patrick <patrick.cardoso@estudante.iftm.edu.br>
2023-10-25 16:05:39 -0300

    addpatrick

--- utils/data.ts
+++ utils/data.ts
@@ -1,5 +1,7 @@
 import Profile from '../public/portrait.jpeg';
 import Pedrao from '../public/pedrao.jpeg'
+import catra from '../public/catra.png'
+
 export const cardData = [
   {
     id: 0,
@@ -38,6 +40,26 @@ export const cardData = [
     src: Pedrao,
     bio: 'Jack of all, Master of some',
     genre: ['Metalcore', 'Pop', 'Rap'],
+    tracks: [
+      {
+        name: "Tears Don't Fall",
+        artist: 'Bullet For my Valentine',
+        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
+      },
+      {
+        name: 'FEEL NOTHING',
+        artist: 'The Plot in You',
+        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Mr Catra',
+    age: 19,
+    src: catra,
+    bio: 'O Fiel, se tornou paizão',
+    genre: ['Funk carioca rap'],
     tracks: [
       {
         name: "Tears Don't Fall",
