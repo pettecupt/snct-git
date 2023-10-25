eaa6a9f6a4385826d7530838947f674a0fd93b59
unknown <pedrohobodart@gmail.com>
2023-10-25 15:50:36 -0300

    add tigrao

--- utils/data.ts
+++ utils/data.ts
@@ -1,112 +1,25 @@
-import Profile from '../public/portrait.jpeg';
-<<<<<<< HEAD
 import tigrao from '../public/tigrao.png'
 export const cardData = [
   {
-    id: 0,
+    id: 4,
     name: 'Tigrão',
     age: 95,
     src: tigrao,
     bio: 'É um caminho perigoso que eu pulo… mas eu pulo sozinho.',
-=======
-import Pedrao from '../public/pedrao.jpeg'
-<<<<<<< HEAD
-import Christyan from '../public/christyan.jpeg'
-import flork from '../public/images.png'
-=======
-import Pernalonga from '../public/christyan.jpeg'
->>>>>>> 08b7b2b27e98a1e02087ca6e5b84dce5232b06b8
-export const cardData = [
-  
-  {
-    id: 0,
-    name: 'dudu',
-    age: 2,
-    src: Pedrao,
-    bio: 'Jack of all, Master of some',
->>>>>>> 08b7b2b27e98a1e02087ca6e5b84dce5232b06b8
-    genre: ['Metalcore', 'Pop', 'Rap'],
+    genre: ['Funk', 'Rock'],
     tracks: [
       {
-        name: "Tears Don't Fall",
-        artist: 'Bullet For my Valentine',
+        name: "Cerol na Mão",
+        artist: 'Bonde do Tigrão',
         img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
       },
       {
-        name: 'FEEL NOTHING',
-        artist: 'The Plot in You',
+        name: 'Eye of the Tiger',
+        artist: 'Survivor',
         img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
       },
     ],
-  },
-  {
-    id: 1,
-    name: 'dudu',
-    age: 2,
-    src: Pedrao,
-    bio: 'Jack of all, Master of some',
-    genre: ['Metalcore', 'Pop', 'Rap'],
-    tracks: [
-      {
-        name: "Tears Don't Fall",
-        artist: 'Bullet For my Valentine',
-        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
-      },
-      {
-        name: 'FEEL NOTHING',
-        artist: 'The Plot in You',
-        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
-      },
-    ],
-  },
-  {
-    id: 2,
-<<<<<<< HEAD
-    name: 'dudu',
-    age: 2,
-    src: Pedrao,
-=======
-    name: 'perna',
-    age: 22,
-    src: Pernalonga,
->>>>>>> 08b7b2b27e98a1e02087ca6e5b84dce5232b06b8
-    bio: 'Jack of all, Master of some',
-    genre: ['Metalcore', 'Pop', 'Rap'],
-    tracks: [
-      {
-        name: "Tears Don't Fall",
-        artist: 'Bullet For my Valentine',
-        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
-      },
-      {
-        name: 'FEEL NOTHING',
-        artist: 'The Plot in You',
-        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
-      },
-    ],
-<<<<<<< HEAD
-  },
-  {
-    id: 3,
-    name: 'Christyan',
-    age: 2,
-    src: flork,
-    bio: 'Cheiroso e Feio',
-    genre: ['Metalcore', 'Pop', 'Rap'],
-    tracks: [
-      {
-        name: "Tear3s Don't Fall",
-        artist: 'Bullet For my Valentine',
-        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
-      },
-      {
-        name: 'FEEL NOTHING',
-        artist: 'The Plot in You',
-        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
-      },
-    ],
-=======
->>>>>>> 08b7b2b27e98a1e02087ca6e5b84dce5232b06b8
+
   }
 
 ]
