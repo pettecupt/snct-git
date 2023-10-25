5b52bdc1475ec3df1b0c67b01bb74d1c946dc716
JoaoSamartinos <joao.samartino@estudante.iftm.edu.br>
2023-10-25 15:20:08 -0300

    J.s

--- utils/data.ts
+++ utils/data.ts
@@ -1,24 +1,17 @@
 import Profile from '../public/portrait.jpeg';
 import Pedrao from '../public/pedrao.jpeg'
+import Christyan from '../public/christyan.jpeg'
+import flork from '../public/images.png'
 export const cardData = [
+  
   {
     id: 0,
-    name: 'Nash',
-    age: 20,
-    src: Profile,
+    name: 'dudu',
+    age: 2,
+    src: Pedrao,
     bio: 'Jack of all, Master of some',
     genre: ['Metalcore', 'Pop', 'Rap'],
     tracks: [
-      {
-        name: 'Blood & Water',
-        artist: 'Memphis May Fire',
-        img: 'https://i.scdn.co/image/ab67616d0000b27336daf308de541e4019a82139',
-      },
-      {
-        name: 'Fighting Gravity',
-        artist: 'Of Mice & Men',
-        img: 'https://i.scdn.co/image/ab67616d0000b2738eaa8ea5ed8b61cfeeda605f',
-      },
       {
         name: "Tears Don't Fall",
         artist: 'Bullet For my Valentine',
@@ -50,5 +43,46 @@ export const cardData = [
         img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
       },
     ],
+  },
+  {
+    id: 2,
+    name: 'dudu',
+    age: 2,
+    src: Pedrao,
+    bio: 'Jack of all, Master of some',
+    genre: ['Metalcore', 'Pop', 'Rap'],
+    tracks: [
+      {
+        name: "Tears Don't Fall",
+        artist: 'Bullet For my Valentine',
+        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
+      },
+      {
+        name: 'FEEL NOTHING',
+        artist: 'The Plot in You',
+        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Christyan',
+    age: 2,
+    src: flork,
+    bio: 'Cheiroso e Feio',
+    genre: ['Metalcore', 'Pop', 'Rap'],
+    tracks: [
+      {
+        name: "Tear3s Don't Fall",
+        artist: 'Bullet For my Valentine',
+        img: 'https://i.scdn.co/image/ab67616d0000b27354113df5ab7a69df8a44c37e',
+      },
+      {
+        name: 'FEEL NOTHING',
+        artist: 'The Plot in You',
+        img: 'https://i.scdn.co/image/ab67616d0000b273197f778e9f68a8ab1d7da3f8',
+      },
+    ],
   }
+
 ]
